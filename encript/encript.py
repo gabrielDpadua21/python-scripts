6e007b03d51bbaeebdacdbac3ff3722aa215f20b
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2025-06-14 03:35:33 -0300

    refactor: add method to clean function

--- encript/encript.py
+++ encript/encript.py
@@ -2,17 +2,29 @@ import subprocess
 
 file_name = 'passlist.txt'
 
+
+def clean_file():
+    command = 'echo > passencript.txt'
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+
+    if result.returncode == 0:
+        print("Clean file passencript.txt")
+        print(result.stdout)
+    else:
+        print("Error to clean file")
+        print(result.stderr)
+
 with open(file_name, 'r') as file:
+    clean_file()
     for line in file:
         if line != '':
           line = line.replace('\n', '').replace('\r\n', '').strip()
-          command = f"echo {line} | md5sum >> passencript.txt"
+          command = f"echo -n {line} | md5sum >> passencript.txt"
           result = subprocess.run(command, shell=True, text=True, capture_output=True)
 
           if result.returncode == 0:
-            print("Saida")
+            print("Add line on file")
             print(result.stdout)
           else:
               print("Error")
               print(result.stderr)
-
